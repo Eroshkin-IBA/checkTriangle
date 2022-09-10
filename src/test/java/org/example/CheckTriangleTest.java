6a81fab5d0d41be62f97534ed7bb7488995367d0
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2022-09-10 15:19:57 +0300

    final commit2

--- src/test/java/org/example/CheckTriangleTest.java
+++ src/test/java/org/example/CheckTriangleTest.java
@@ -27,21 +27,21 @@ class CheckTriangleTest {
     @Test
     void isNegative() {
         System.out.println("Тест на негативные значения");
-        CheckTriangle checkTriangle = new CheckTriangle(1,-5,6);
+        CheckTriangle checkTriangle = new CheckTriangle(56,-89,21);
         assertFalse(checkTriangle.isPositive());
     }
 
     @Test
     void isTriangle() {
         System.out.println("Тест на треугольник");
-        CheckTriangle checkTriangle = new CheckTriangle(2.9f,6,5);
+        CheckTriangle checkTriangle = new CheckTriangle(68.98f,75.36f,94.55f);
         assertTrue(checkTriangle.isTriangle());
     }
 
     @Test
     void isEquilateralTriangle() {
         System.out.println("Тест на равносторонний треугольник");
-        CheckTriangle checkTriangle = new CheckTriangle(3,3,3);
+        CheckTriangle checkTriangle = new CheckTriangle(6,6,6);
         assertTrue(checkTriangle.isEquilateralTriangle());
     }
 
@@ -50,6 +50,20 @@ class CheckTriangleTest {
         System.out.println("Тест на равнобедренный треугольник");
         CheckTriangle checkTriangle = new CheckTriangle(2.9f,6,6);
         assertTrue(checkTriangle.isIsoscelesTriangle());
+    }
+
+    @Test
+    void isEqualsNull() {
+        System.out.println("Тест на нулевое значение");
+        CheckTriangle checkTriangle = new CheckTriangle(0,5,6);
+        assertFalse(checkTriangle.isPositive());
+    }
+
+    @Test
+    void isNotTriangle() {
+        System.out.println("Тест на треугольник");
+        CheckTriangle checkTriangle = new CheckTriangle(15,100,26);
+        assertFalse(checkTriangle.isTriangle());
     }
 
 
